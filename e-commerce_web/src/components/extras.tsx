920aaa1176c170332594cb2b1a787e61cf708721
Blessen25 <blessenvinoymathew25@gmail.com>
2025-11-26 11:20:07 +1100

    Changes in sale-banner

--- e-commerce_web/src/components/extras.tsx
+++ e-commerce_web/src/components/extras.tsx
@@ -4,9 +4,9 @@ const SaleBanner = () => {
 
     return(
         <>
-            <div className="sale-banner ">
+            <div className="sale-banner">
                 <h1 className='para-small font-wei-lighter'>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</h1>
-                <a href="#" className='para-small' >Shop Now</a>
+                <a href="#" className='para-small'>Shop Now</a>
             </div>
         </>
     )
