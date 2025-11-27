0c33bcf0b7e83dc6c106898b52deb8dc0638e138
Blessen25 <blessenvinoymathew25@gmail.com>
2025-11-27 14:26:49 +1100

    Added underline for shop now while hovering.

--- e-commerce_web/src/components/extras.tsx
+++ e-commerce_web/src/components/extras.tsx
@@ -6,7 +6,7 @@ const SaleBanner = () => {
         <>
             <div className="sale-banner">
                 <h1 className='para-small font-wei-lighter'>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</h1>
-                <a href="#" className='para-small'>Shop Now</a>
+                <a href="#" className='para-small shop_now'>Shop Now</a>
             </div>
         </>
     )
