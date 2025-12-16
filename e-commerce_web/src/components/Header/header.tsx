6c9659b0f1291e16ea996a5302ff6f04ce677a62
Blessen25 <blessenvinoymathew25@gmail.com>
2025-12-16 13:09:18 +1100

    Some tweaks

--- e-commerce_web/src/components/Header/header.tsx
+++ e-commerce_web/src/components/Header/header.tsx
@@ -54,6 +54,7 @@ const HeaderComp = () => {
                                         <div className="sidebar">
                                             
                                         </div>
+                                        
                                     </>
                                 )}
                             </>
